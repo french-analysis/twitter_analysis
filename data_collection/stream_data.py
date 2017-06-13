3d2465b84b9261ad1e6e59859e2c59d411bae7b6
challier-08 <challier.victor@gmail.com>
2017-06-14 00:08:36 +0200

    enhance tstreaming script

--- data_collection/stream_data.py
+++ data_collection/stream_data.py
@@ -3,23 +3,17 @@ from tweepy import OAuthHandler
 from config import consumer_key, consumer_secret, access_token, access_secret
 from tweepy import Stream
 from tweepy.streaming import StreamListener
- 
-consumer_key = consumer_key 
-consumer_secret = consumer_secret
-access_token = access_token
-access_secret = access_secret
- 
-auth = OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_secret)
- 
-api = tweepy.API(auth)
 
-class MyListener(StreamListener):
- 
+class tweetListener(StreamListener): 
+
+    def __init__(self, max_num_tweets):
+        self.counter = 0
+        self.max_num_tweets = max_num_tweets
+
     def on_data(self, data):
         try:
             with open('python.json', 'a') as f:
-                f.write(data)
+                f.write(data['text'])
                 return True
         except BaseException as e:
             print("Error on_data: %s" % str(e))
@@ -28,6 +22,25 @@ class MyListener(StreamListener):
     def on_error(self, status):
         print(status)
         return True
- 
-twitter_stream = Stream(auth, MyListener())
-twitter_stream.filter(track=['#nadal'])
+
+if __name__ == "__main__":
+
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_secret)
+    twitter_api = tweepy.API(auth)
+
+    # Search stuff
+    search_results = tweepy.Cursor(twitter_api.search, q="Python").items(5)
+    for result in search_results:
+        print(result.text)
+
+    trends = twitter_api.trends_place(1)
+
+    for trend in trends[0]["trends"]:
+        print(trend['name'])
+
+    twitter_stream = Stream(auth, twitter_listener(num_tweets_to_grab=10))
+    try:
+        twitter_stream.sample()
+    except Exception as e:
+        print(e.__doc__) 
