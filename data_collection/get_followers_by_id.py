9d2a44af94084dedfcfa237c4a3a2cd2ed27e91b
challier-08 <challier.victor@gmail.com>
2017-06-14 09:12:50 +0200

    modif on followers

--- data_collection/get_followers_by_id.py
+++ data_collection/get_followers_by_id.py
@@ -3,19 +3,17 @@ import tweepy
 from tweepy import OAuthHandler
 from config import consumer_key, consumer_secret, access_token, access_secret
 
-consumer_key = consumer_key
-consumer_secret = consumer_secret
-access_token = access_token
-access_secret = access_secret
+if __name__ == "__main__":
 
-auth = OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_secret)
-
-api = tweepy.API(auth)
-
-ids = []
-for page in tweepy.Cursor(api.followers_ids, screen_name="@vbd2017").pages():
-    ids.extend(page)
-    #time.sleep(60)
-
-print(ids)
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_secret)
+    twitter_api = tweepy.API(auth)
+    
+    ids = []
+    for page in tweepy.Cursor(twitter_api.followers_ids, screen_name="@vbd2017").pages():
+        ids.extend(page)
+        #time.sleep(60)
+    
+    with open('followers_ids.json', 'a') as f:
+        f.write(''.join(str(e) for e in ids))
+    print(ids)
